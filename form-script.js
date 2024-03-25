3ab1fb74cbdbb1a3ab70965a5868b4a11e983c9d
codystanley <cody.stanley@triagelogic.com>
2024-03-25 19:41:26 -0400

    Updated to add additional configurations.

--- form-script.js
+++ form-script.js
@@ -1,21 +1,25 @@
-/**
- * This script controls the behavior of a form, including showing and hiding fields based on user interactions.
- * It also handles the activation and deactivation of a search modal.
- *
- * @file This file contains the JavaScript code for the form script.
- * @summary Controls the behavior of a form and manages the search modal.
- * @description The script initializes the form by hiding certain fields and setting up event listeners.
- * It also handles the "Next" button click event, showing the search modal if necessary, and updating the visibility of fields accordingly.
- * @requires jQuery
- * @version 1.0
- * @since 2022-10-01
- */
-let showModal = false; // Activate/Deactive the search modal
 
-$(document).ready(function() {
-    // Initially hide the fields
+/**
+ * This script controls the form behavior and configuration settings.
+ * It handles the visibility of form fields based on checkbox states and button clicks.
+ * The script also includes event listeners for checkbox changes and button clicks.
+ * 
+ * @file This file is located at /Users/codystanley/Documents/apps/intake_mockup_AS_P1/form-script.js
+ * @version 1.0.0
+ */
+
+// Set checkbox configurations to false.
+let phoneDatabase = false;
+let phoneResult = false;
+let patientDatabase = false;
+let patientResult = false;
+
+$(document).ready(function()
+{
+    // Initially hide all of the fields except 'Phone Number'
     $("#dobDiv").hide();
     $("#ptLastNameDiv").hide();
+    $("#nextPatientButtonDiv").hide();
     $("#ptFirstNameDiv").hide();
     $("#ptBiologicalSexDiv").hide();
     $("#patientCallerDiv").hide();
@@ -25,23 +29,88 @@ $(document).ready(function() {
     $("#ptLocationDiv").hide();
     $("#submitButtonDiv").hide();
 
-    // Update showModal based on initial checkbox state
-    showModal = $('#showModalCheckbox').is(':checked');
+    // Update configuration based on initial checkbox states
+    phoneDatabase = $('#configPhoneCheckbox').is(':checked');
+    phoneResult = $('#configPhoneResultsCheckbox').is(':checked');
+    patientDatabase = $('#configPatientCheckbox').is(':checked');
+    patientResult = $('#configPatientResultsCheckbox').is(':checked');
 
-    // Event listener for the checkbox
-    $('#showModalCheckbox').change(function() {
-    showModal = $(this).is(':checked');
+    // Event listeners for the Phone Database checkboxes
+    $('#configPhoneCheckbox').change(function()
+    {
+        phoneDatabase = $(this).is(':checked');
     });
 
-    // "Next" button click handler
-    $("#nextButton").click(function() {
-        if(showModal) {
-            $("#confirmModal").modal('show'); // Show the modal
+    $('#configPhoneResultsCheckbox').change(function()
+    {
+        phoneResult = $(this).is(':checked');
+    });
+
+    $('#configPatientCheckbox').change(function()
+    {
+        patientDatabase = $(this).is(':checked');
+    });
+
+    $('#configPatientResultsCheckbox').change(function()
+    {
+        patientResult = $(this).is(':checked');
+    });
+
+
+    // "Next" after phone number entry.
+    $("#nextPhoneButton").click(function()
+    {
+        if(phoneDatabase && phoneResult)
+        {
+            $("#confirmPhoneModal").modal('show');
+
+            // Confirm phone number patient button handler
+            $("#phoneBtnConfirm").click(function() {
+                $("#confirmPhoneModal").modal('hide');
+                $("#nextPhoneButtonDiv").hide();
+                $("#dobDiv").show();
+                $("#ptLastNameDiv").show();
+                $("#nextPatientButtonDiv").hide();
+                $("#ptFirstNameDiv").show();
+                $("#ptBiologicalSexDiv").show();
+                $("#patientCallerDiv").show();
+                $("#callerLastNameDiv").show();
+                $("#callerFirstNameDiv").show();
+                $("#callerRelationshipDiv").show();
+                $("#ptLocationDiv").show();
+                $("#submitButtonDiv").show();
+
+                // Add dummy data
+                $("#dob").val("01/01/2000");
+                $("#ptLastName").val("Doe");
+                $("#ptFirstName").val("John");
+                $("#ptBiologicSex").val("M");
+                $("#ptLocation").val("NY");
+                $("#patientCaller").focus();
+            });
+        } else
+        {
+            $("#nextPhoneButtonDiv").hide();
+            $("#dobDiv").show();
+            $("#ptLastNameDiv").show();
+            $("#nextPatientButtonDiv").show();
+        }
+    });
+
+    // "Next Patient" button click handler
+    $("#nextPatientButton").click(function()
+    {
+        if(patientDatabase && patientResult)
+        {
+            $("#confirmPatientModal").modal('show'); // Show the modal
 
             // Confirmation Modal "Confirm" button handler
-            $("#btnConfirm").click(function() {
-                $("#confirmModal").modal('hide'); // Hide the modal
-                $("#nextButtonRow").hide();
+            $("#patientBtnConfirm").click(function() {
+                $("#confirmPatientModal").modal('hide');
+                $("#nextPhoneButtonDiv").hide();
+                $("#dobDiv").show();
+                $("#ptLastNameDiv").show();
+                $("#nextPatientButtonDiv").hide();
                 $("#ptFirstNameDiv").show();
                 $("#ptBiologicalSexDiv").show();
                 $("#patientCallerDiv").show();
@@ -50,10 +119,23 @@ $(document).ready(function() {
                 $("#callerRelationshipDiv").show();
                 $("#ptLocationDiv").show();
                 $("#submitButtonDiv").show();
-            })
-        }   else {
-                // "Next" button click handler
-                $("#nextButtonRow").hide();
+
+                // Add dummy data
+                $("#dob").val("01/01/2020");
+                $("#ptLastName").val("Doe");
+                $("#ptFirstName").val("Jane");
+                $("#ptBiologicSex").val("F");
+                $("#ptLocation").val("FL");
+                $("#patientCaller").focus();
+            });
+        } else
+        {
+            // "Next" button click handler
+            $("#confirmPatientModal").modal('hide');
+                $("#nextPhoneButtonDiv").hide();
+                $("#dobDiv").show();
+                $("#ptLastNameDiv").show();
+                $("#nextPatientButtonDiv").hide();
                 $("#ptFirstNameDiv").show();
                 $("#ptBiologicalSexDiv").show();
                 $("#patientCallerDiv").show();
@@ -62,6 +144,25 @@ $(document).ready(function() {
                 $("#callerRelationshipDiv").show();
                 $("#ptLocationDiv").show();
                 $("#submitButtonDiv").show();
-            }
-        })
-    })
+        }
+    });
+
+    /**
+     * Event handler for the 'change' event of the '#patientCaller' select element.
+     *
+     * When the selected value is "Patient", this function sets the values of
+     * '#callerLastName', '#callerFirstName', and '#callerRelationship' to "same".
+     *
+     * @listens #patientCaller:change
+     */
+    $("#patientCaller").change(function() {
+
+        if ($(this).val() === "Patient") {
+            $("#callerLastName").val("same");
+            $("#callerFirstName").val("same");
+            $("#callerRelationship").val("same");
+        }
+
+    });
+
+})
